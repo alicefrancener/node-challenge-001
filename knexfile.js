5c93b0d1c41aad6c916f8ef74b19c26b89d91d09
Alice Francener <alicefrancener@gmail.com>
2021-03-22 10:55:37 -0300

    chore: add knex config

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      user: process.env.POSTGRES_USER
+    },
+    migrations: {
+      directory: './src/database/migrations/'
+    }
+  }
+};
