f93a8b0a248821edcf654cf16327b50f12f7d160
Alice Francener <alicefrancener@gmail.com>
2021-03-24 17:37:09 -0300

    feat: routers to /articles

--- src/routers/articles.js
+++ src/routers/articles.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const ArticleController = require('./../controllers/articles');
 
 const router = express.Router();
 
-const Article = require('./../models/articles');
+const articleController = new ArticleController();
 
-router.get('/', async (req, res) => {
-  const articles = await Article.query().select('title');
-  res.json(articles);
-});
+router.get('/articles/:id', articleController.showById);
+// TODO router.get('/articles?category=:slug', articleController.showByCategory);
+
+router.get('/admin/articles', articleController.index);
+// TODO router.post('/admin/articles', articleController.create);
+// TODO router.put('/admin/articles', articleController.update);
+// TODO router.delete('/admin/articles', articleController.destroy);
 
 module.exports = router;
