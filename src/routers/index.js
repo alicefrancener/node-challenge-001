d73f10891f7f820691166c21aae94b57ff9610c4
Alice Francener <alicefrancener@gmail.com>
2021-03-26 09:13:29 -0300

    feat: add auth route

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const auth = require('./auth');
 const articles = require('./articles');
 
 const router = express.Router();
 
+router.use(auth);
 router.use(articles);
 
 module.exports = router;
