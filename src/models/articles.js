d3c6aeae190840abec648eb3dff884e981af170b
Alice Francener <alicefrancener@gmail.com>
2021-03-26 12:32:06 -0300

    feat: add json schema to article

--- src/models/articles.js
+++ src/models/articles.js
@@ -8,6 +8,25 @@ class Article extends Model {
   static get tableName() {
     return 'article';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        id: { type: 'integer' },
+        title: { type: 'string', maxLength: 1000},
+        category: { type: 'string', maxLength: 100},
+        summary: { type: 'string', maxLength: 1000},
+        first_paragraph: { type: 'string'},
+        body: { type: 'string'},
+        created_at: { type: 'string'},
+        updated_at: { type: 'string'},
+        author_id: { type: 'integer'}
+      },
+      required: ['name', 'picture'],
+      additionalPropertires: false,
+    };
+  }
 }
 
 module.exports = Article;
