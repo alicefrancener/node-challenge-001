70accc031e03186b8293253608f5d770940bb02b
Alice Francener <alicefrancener@gmail.com>
2021-03-26 09:08:36 -0300

    feat: creat json schema for user

--- src/models/users.js
+++ src/models/users.js
@@ -8,6 +8,21 @@ class User extends Model {
   static get tableName() {
     return 'user';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        id: { type: 'integer' },
+        email: { type: 'string', minLength: 1, maxLength: 255, pattern: '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'},
+        password: { type: 'string', minLength: 8, maxLength: 100},
+        created_at: { type: 'string'},
+        updated_at: { type: 'string'}
+      },
+      required: ['email', 'password'],
+      additionalPropertires: false,
+    };
+  }
 }
 
 module.exports = User;
