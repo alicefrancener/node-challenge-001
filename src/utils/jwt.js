a0b5f50ed74471b7e6cc84f1e01e8ef602d4621b
Alice Francener <alicefrancener@gmail.com>
2021-03-26 18:12:56 -0300

    feat: verify token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,13 +2,27 @@ const jwt = require('jsonwebtoken');
 
 const sign = (payload) => {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1d'
-    }, (error, token) => {
-      if(error) return reject(error);
-      return resolve(token);
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '1d',
+      },
+      (error, token) => {
+        if (error) return reject(error);
+        return resolve(token);
+      }
+    );
+  });
+};
+
+const verify = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) return reject(error);
+      return resolve(decoded);
     });
   });
 };
 
-module.exports = { sign };
+module.exports = { sign , verify};
