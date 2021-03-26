755059c4672cd6fb7e42c8da939cd281437096c5
Alice Francener <alicefrancener@gmail.com>
2021-03-26 10:37:32 -0300

    feat: add json schema for authors

--- src/models/authors.js
+++ src/models/authors.js
@@ -8,6 +8,21 @@ class Author extends Model {
   static get tableName() {
     return 'author';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 2, maxLength: 255},
+        picture: { type: 'string', maxLength: 2000},
+        created_at: { type: 'string'},
+        updated_at: { type: 'string'}
+      },
+      required: ['name', 'picture'],
+      additionalPropertires: false,
+    };
+  }
 }
 
 module.exports = Author;
