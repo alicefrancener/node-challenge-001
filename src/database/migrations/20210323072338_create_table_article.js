943f92e739422399eb1aaa8f62abfa6bb5e1fcbe
Alice Francener <alicefrancener@gmail.com>
2021-03-26 12:17:41 -0300

    refactor: table article

--- src/database/migrations/20210323072338_create_table_article.js
+++ src/database/migrations/20210323072338_create_table_article.js
@@ -4,12 +4,12 @@ exports.up = (knex) => {
   return knex.schema.createTable(tableName, (table) => {
     table.increments().primary();
     table.string('title', 1000).notNullable();
+    table.string('category', 100).notNullable();
     table.string('summary', 1000).notNullable();
     table.text('first_paragraph').notNullable();
     table.text('body').notNullable();
     table.timestamps(false, true);
     table.integer('author_id').unsigned().references('id').inTable('author').onUpdate('CASCADE').onDelete('CASCADE');
-    table.integer('category_id').unsigned().references('id').inTable('category').onUpdate('CASCADE');
   });
 };
 
