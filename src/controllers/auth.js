a824a5c77bb65c225bb50747698c95d43f22ed2c
Alice Francener <alicefrancener@gmail.com>
2021-03-26 14:27:40 -0300

    style: format code

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,24 +8,28 @@ class AuthController {
     const { email, password } = req.body;
 
     try {
-      const passwordPattern = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}');
-      if(!passwordPattern.test(password)) {
-        const error = new Error('Password must have: at least one digit (0-9), at least one lowercase character (a-z), at least one uppercase character (A-Z), at least 8 characters');
+      const passwordPattern = new RegExp(
+        '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}'
+      );
+      if (!passwordPattern.test(password)) {
+        const error = new Error(
+          'Password must have: at least one digit (0-9), at least one lowercase character (a-z), at least one uppercase character (A-Z), at least 8 characters'
+        );
         res.status(400);
         throw error;
       }
 
       const hashedPass = await bcrypt.hash(password, 12);
-      const user = await User.query().insert({email, password: hashedPass});
+      const user = await User.query().insert({ email, password: hashedPass });
 
-      const token = await jwt.sign({id: user.id, email});
-      res.status(201).send({id : user.id, email, token});
+      const token = await jwt.sign({ id: user.id, email });
+      res.status(201).send({ id: user.id, email, token });
     } catch (error) {
-      if(error instanceof UniqueViolationError) {
+      if (error instanceof UniqueViolationError) {
         error.message = 'Email already registered.';
         res.status(409);
       }
-      if(error instanceof ValidationError) {
+      if (error instanceof ValidationError) {
         res.status(400);
       }
 
@@ -50,8 +54,8 @@ class AuthController {
         throw error;
       }
 
-      const token = await jwt.sign({id: user.id, email});
-      res.json({id : user.id, email, token});
+      const token = await jwt.sign({ id: user.id, email });
+      res.json({ id: user.id, email, token });
     } catch (error) {
       next(error);
     }
