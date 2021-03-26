5139db0c6b5e02ce09a5723305a96fbc11fc9f04
Alice Francener <alicefrancener@gmail.com>
2021-03-26 14:27:14 -0300

    feat: route to api/articles?category=:slug

--- src/routers/articles.js
+++ src/routers/articles.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const articleController = new ArticleController();
 
 router.get('/articles/:id', articleController.showById);
-// TODO router.get('/articles?category=:slug', articleController.showByCategory);
+router.get('/articles/', articleController.showByCategory);
 
 router.get('/admin/articles', articleController.index);
 // TODO router.post('/admin/articles', articleController.create);
