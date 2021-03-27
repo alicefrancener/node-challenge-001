20320fdb9ca5007388b7ec6dd668f4bde616d260
Alice Francener <alicefrancener@gmail.com>
2021-03-27 16:41:54 -0300

    chore: configure coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   globalSetup: './src/setupTests.js',
   globalTeardown: './src/teardownTests.js',
   testEnvironment: 'node',
+  verbose: true,
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/database/migrations/**',
+    '!**/database/seeds/**',
+  ]
 };
