64c11b0fbed5b9dff99cdbd68c0882a0d7b80fb5
Alice Francener <alicefrancener@gmail.com>
2021-03-27 14:37:06 -0300

    refactor: remove status code from response body message

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,6 @@ const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
-    status: error.statusCode,
     error: error.name,
     message: error.message,
   });
