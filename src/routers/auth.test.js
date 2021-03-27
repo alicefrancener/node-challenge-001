c1e92c5526bfb7dbb9fee87525317c18cf6cb2a4
Alice Francener <alicefrancener@gmail.com>
2021-03-27 14:36:27 -0300

    test: request to endpoint api/sign-up

---
+++ src/routers/auth.test.js
@@ -0,0 +1,56 @@
+const supertest = require('supertest');
+
+const app = require('../app');
+const request = supertest(app);
+
+describe('POST /api/sign-up', () => {
+  it('should respond with an email validation error ', async (done) => {
+    const response = await request
+      .post('/api/sign-up')
+      .send({
+        'email': 'usuario@.br',
+        'password': 'pass123IOKJ1234'
+      });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('email: should match pattern "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"');
+    done();
+  });
+
+  it('should respond with a password validation error ', async (done) => {
+    const response = await request
+      .post('/api/sign-up')
+      .send({
+        'email': 'usuario@com.br',
+        'password': 'pass123'
+      });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('Password must have: at least one digit (0-9), at least one lowercase character (a-z), at least one uppercase character (A-Z), at least 8 characters');
+    done();
+  });
+
+  it('should respond with an user and token ', async (done) => {
+    const response = await request
+      .post('/api/sign-up')
+      .send({
+        'email': 'usuario@com.br',
+        'password': 'pass123PASS'
+      });
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty(['user','id']);
+    expect(response.body).toHaveProperty(['user', 'email']);
+    expect(response.body).toHaveProperty('token');
+    done();
+  });
+
+  it('should respond with an error (user already registered) ', async (done) => {
+    const response = await request
+      .post('/api/sign-up')
+      .send({
+        'email': 'usuario@com.br',
+        'password': 'pass123PASSa'
+      });
+    expect(response.status).toBe(409);
+    expect(response.body.message).toBe('Email already registered.');
+    done();
+  });
+});
