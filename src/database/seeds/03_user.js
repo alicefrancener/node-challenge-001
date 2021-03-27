41f121a1ab306d63f51f67fa82162eb104e1eb92
Alice Francener <alicefrancener@gmail.com>
2021-03-27 16:43:09 -0300

    refactor: encrypt seeds password

--- src/database/seeds/03_user.js
+++ src/database/seeds/03_user.js
@@ -1,12 +1,15 @@
+const bcrypt = require('bcrypt');
+
 const tableName = 'user';
 
-exports.seed = (knex) => {
-  return knex(tableName)
+exports.seed = async (knex) => {
+  const hashedPass = await bcrypt.hash('pass123PASS', 12);
+  await knex(tableName)
     .del()
     .then(() => {
       return knex(tableName).insert([
-        {email: 'example01@example.com', password: 'pass123'},
-        {email: 'example02@example.com', password: 'pass123', is_admin: true},
+        {email: 'admin@example.com', password: hashedPass, is_admin: true},
+        {email: 'user@example.com', password: hashedPass}
       ]);
     });
 };
