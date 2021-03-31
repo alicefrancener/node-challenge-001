0c9ca7d895aaf156a7818b7269ca580f245fe212
Alice Francener <alicefrancener@gmail.com>
2021-03-30 22:05:21 -0300

    feat: add routers to api/admin/authors

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const auth = require('./auth');
 const articles = require('./articles');
+const authors = require('./authors');
 
 const router = express.Router();
 
 router.use(auth);
 router.use(articles);
+router.use(authors);
 
 module.exports = router;
